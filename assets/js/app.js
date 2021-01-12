d9b97e478317c9338801d87519fad7bbe056fd11
Tijani-zainab <jani.tijj@gmail.com>
2021-01-12 17:12:01 +0100

    calculator first commit

---
+++ assets/js/app.js
@@ -0,0 +1,151 @@
+
+//Calculator
+
+
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+
+};
+
+function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+    console.log(calculator);
+}
+
+function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+  
+    if (waitingForSecondOperand === true) {
+      calculator.displayValue = digit;
+      calculator.waitingForSecondOperand = false;
+    } else {
+      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+  
+    console.log(calculator);
+  }
+  
+
+function inputDecimal(dot) {
+  
+  if (calculator.waitingForSecondOperand === true) {
+  	calculator.displayValue = '0.'
+    calculator.waitingForSecondOperand = false;
+    return
+  }
+    // If the `displayValue` property does not contain a decimal point
+    if (!calculator.displayValue.includes(dot)) {
+      // Append the decimal point
+      calculator.displayValue += dot;
+    }
+}  
+  
+function handleOperator(nextOperator) {
+    // Destructure the properties on the calculator object
+    const { firstOperand, displayValue, operator } = calculator
+    // `parseFloat` converts the string contents of `displayValue`
+    // to a floating-point number
+    const inputValue = parseFloat(displayValue);
+    if (operator && calculator.waitingForSecondOperand)  {
+      calculator.operator = nextOperator;
+      console.log(calculator);
+      return;
+    }
+  
+    // verify that `firstOperand` is null and that the `inputValue`
+    // is not a `NaN` value
+    if (firstOperand === null && !isNaN(inputValue)) {
+      // Update the firstOperand property
+      calculator.firstOperand = inputValue;
+    }   else if (operator) {
+      const result = calculate(firstOperand, inputValue, operator);
+  
+      calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+      calculator.firstOperand = result;
+    }
+  
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+
+    console.log(calculator);
+}
+
+function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+        return firstOperand + secondOperand;
+    } else if (operator === '-') {
+        return firstOperand - secondOperand;
+    } else if (operator === '*') {
+        return firstOperand * secondOperand;
+    } else if (operator === '/') {
+        return firstOperand / secondOperand;
+    } else if (operator === '%') {
+        return firstOperand / 100;
+    }
+  
+    return secondOperand;
+}  
+
+
+function updateDisplay() {
+    // select the element with class of `calculator-screen`
+    const display = document.querySelector('.text-area');
+    // update the value of the element with the contents of `displayValue`
+    display.value = calculator.displayValue;
+}
+  
+  updateDisplay();
+
+  const keys = document.querySelector('.calc__keys');
+    keys.addEventListener('click', (event) => {
+    // Access the clicked element
+    const { target } = event;
+
+    // Check if the clicked element is a button.
+    // If not, exit from the function
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if (target.classList.contains('operators')) {
+        handleOperator(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('all-clear')) {
+      resetCalculator();
+      updateDisplay();      
+      return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();    
+
+});
+
+
+/*
+function generateJoke() {
+  var config = {
+    headers: { Accept: 'application/json'}
+  }
+  fetch('https://icanhazdadjoke.com/', config) .then ((response) => response.json()).then((data) => console.log(data))
+
+  console.log(joke)
+}
+
+generateJoke();
+*/
